9008cb08fcb36ffb16bbe621e348aec989a17dc0
EyeMLeo <Leonid.Akuta@gmail.com>
2022-11-21 15:33:14 +0100

    uzd2 html complete

--- src/App.js
+++ src/App.js
@@ -1,95 +1,110 @@
 import './App.css';
-// #1 budas itraukti paveiksleli
-import house5 from './house5.jpg';
-
-const card1 = (
-  <div className='card'>
-    <h4 className='card__title'>house 5</h4>
-    <img className='card__img' src={house5} alt='namas penktas' />
-  </div>
-);
-const card2 = (
-  <div className='card'>
-    <h4 className='card__title'>house 2</h4>
-    <img className='card__img' src='/img/house2.jpg' alt='namas antras' />
-  </div>
-);
-
-const member1 = (
-  <div className='teamItem'>
-  <img className='card__img' src='/img/team2.jpg' alt='team member' />
-  <h3 className='team__title'>John Doe</h3>
-  <p className='team__ocupation'>CEO & Founder</p>
-  <p className='team_desc'>
-    Phasellus eget enim eu lectus faucibus vestibulum. Suspendisse sodales pellentesque
-    elementum.
-  </p>
-  <button>Contact</button>
-</div>
-);
-
-
 
 // react componentas
-function HouseGrid() {
-  return (
-    <div className='grid1'>
-      {card1}
-      {card2}
-      {card1}
-      {card2}
-      {card1}
-      {card2}
+
+function Logo() {
+  return(
+    <div className="container">
+      <div className="logo"><h2>LOGO</h2></div>
     </div>
-  );
-}
+  )
+  }
 
-function TeamItem(){
+function Header() {
 return(
-  <div className='grid1'>
-    {member1}
-    {member1}
-    {member1}
-    {member1}
-    {member1}
-    {member1}
-    {member1}
-    {member1}
-</div>
+  <div className="hero">
+    <div className="container">
+      <Nav />
+    </div>
+  </div>
 )
 }
-function FormArea(){
-return(
-  <div className='formContainer'>
-    <h3>Form</h3>
-    <form action="">
-      <input type="text" placeholder='Name' />
-      <input type="text" placeholder='Email' />
-      <input type="text" placeholder='Subject' />
-      <input type="text" placeholder='Comment' />
-      <button>Send messege</button>
-    </form>
 
-</div>
-)
+function Nav(){
+  return(
+    <nav>
+    <ul>
+      <li>Home</li>
+      <li>Product</li>
+      <li>Company</li>
+      <li>Contact</li>
+    </ul>
+  </nav>
+  )
 }
+
+function Hero() {
+  return(
+    <header>
+      <div className="container">
+        <h2>hero IMG</h2>
+      </div>
+    </header>
+  )
+  }
+
+  function Main() {
+    return(
+      <main>
+        <div className="container">
+          <MainArticle />
+          <MainArticle />
+          <MainArticle />
+        </div>
+      </main>
+    )
+    }
+
+    function MainArticle() {
+      return(
+        <section>
+          <h2>About</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid iure, voluptates tempore qui doloremque accusamus magnam aut error nemo nostrum!</p>
+        </section>
+      )
+      }
+
+
+    function Footer() {
+      return(
+        <footer>
+          <div className="container">
+            <section>
+              <h2>Content</h2>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quae voluptate ab minima nemo sit, reprehenderit porro voluptatibus tempora inventore fuga ea accusantium dolor a quia, vel perspiciatis! Minima, velit repellat.</p>
+              <h3>Content</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est dolorem iste facere doloremque alias dolore, rerum consequuntur amet dicta sint nam exercitationem iure autem fugiat quas recusandae non suscipit rem excepturi mollitia facilis, reiciendis culpa asperiores. Deleniti molestias numquam quo.</p>
+            </section>
+            <div className="footerNavigation">
+              <h3>Navigation</h3>
+              <Nav />
+            </div>
+          </div>
+        </footer>
+      )
+      }
+
 
 function App() {
-  const name = 'James';
 
   return (
     <div className='App'>
-      <h1 className='mainTitle'>
-        Hello111 {name} !!! {150 * 45}
-      </h1>
-      {/* {houseGrid} */}
-      <HouseGrid />
 
-      {/* <TeamItem /> */}
-      <TeamItem />
+       {/* <Logo /> */}
+        <Logo />
 
-      {/* <FormArea /> */}
-      <FormArea />
+      {/* <Header /> */}
+      <Header />
+
+
+      {/* <Hero /> */}
+      <Hero />
+
+      {/* <Main /> */}
+      <Main />
+
+      {/* <Footer /> */}
+      <Footer />
 
     </div>
   );
